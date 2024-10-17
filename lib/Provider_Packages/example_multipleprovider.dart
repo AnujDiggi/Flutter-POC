a1249a3982a58b9a75a492c0d26001b4171c27ed
Anuj Kumar <anuj.kumar@diggibyte.com>
2024-10-17 17:25:59 +0530

    pub.dev Extracting Widgets

--- lib/Provider_Packages/example_multipleprovider.dart
+++ lib/Provider_Packages/example_multipleprovider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class ExampleMultipleprovider with ChangeNotifier {
     
-    double _value = 1.0;
+   final double _value = 1.0;
     double get value => _value;
 
 }
